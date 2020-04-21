200092d984b000382e7f636b80a421d21b2d2e1d
Jeffrey Lansford <lansfordjr14@gmail.com>
2020-04-20 18:05:43 -0600

    added something to get the server to show something

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require("express");
 const app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+var port = 3000;
 
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-   res.sendFile(__dirname + "public/login.html");
+   res.sendFile(__dirname + "public/index.html");
 });
 
-io.on("connection", function (socket) {
-   console.log(" a user has connected");
-});
-
-http.listen(3000, function () {
-   console.log("listening on port 3000");
+http.listen(port, function () {
+   console.log(`listening on port ${port}`);
 });
