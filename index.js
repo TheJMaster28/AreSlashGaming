3f22c573df0523534d15bd143d366428e5aa58d6
Jeffrey Lansford <lansfordjr14@gmail.com>
2020-04-21 20:01:32 -0600

    added some socket.io stuff to have server and client talk to each other

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-
-const express = require("express");
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const googleConfig = require("./OAuth/config.js");
-var http = require("http").createServer(app);
-var io = require("socket.io")(http);
+const googleConfig = require('./OAuth/config.js');
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var port = 3000;
 
-var User = require("./models/user");
+var User = require('./models/user');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", function (req, res) {
-   res.sendFile(__dirname + "public/index.html");
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + 'public/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user has connected');
+    io.emit('hello_world');
+
+    io.on('login', function () {
+        console.log('a user wants to login');
+    });
 });
 
 http.listen(port, function () {
-   console.log(`listening on port ${port}`);
+    console.log(`listening on port ${port}`);
 });
-
