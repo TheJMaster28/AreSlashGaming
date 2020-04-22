2a3979b5b4fe463ba6cccbb063bf7c36e743bcc2
Ramon Ramirez <39835725+RRamirez1414@users.noreply.github.com>
2020-04-21 18:45:05 -0600

    added mongoose node package

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+const app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+   res.sendFile(__dirname + "public/index.html");
+});
+
+http.listen(port, function () {
+   console.log(`listening on port ${port}`);
+});
+
