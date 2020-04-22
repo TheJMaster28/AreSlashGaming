d960854ac6b5884ff7671d236a95cbb9642d4256
Ramon Ramirez <39835725+RRamirez1414@users.noreply.github.com>
2020-04-21 19:31:53 -0600

    added user config file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const googleConfig = require("./OAuth/config.js");
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 var port = 3000;
+
+var User = require("./models/user");
 
 app.use(express.static("public"));
 
---
+++ models/user.js
@@ -0,0 +1,5 @@
+const User = {
+    "googleId": null,
+    "googleProfile": null,
+    "googleAccessToken": null
+}
