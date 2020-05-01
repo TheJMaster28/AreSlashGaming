8df7d1a1bb49c7f91f9dbef61f6761fe653d543c
Jeffrey Lansford <lansfordjr14@gmail.com>
2020-04-30 20:02:40 -0600

    added socket.io implemantation to Create Post

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 var port = 3000;
 
 /* google OAuth set up */
-const googleConfig = require("./OAuth/config");
+const googleConfig = require('./OAuth/config');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var User = require('./models/user');
@@ -14,7 +14,7 @@ var User = require('./models/user');
 app.use(express.static('public'));
 
 /* passport set up */
-const passport = require("passport");
+const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -24,20 +24,27 @@ io.on('connection', function (socket) {
 
     socket.on('login', function () {
         console.log('a user wants to login');
-        var googleStrategy = new GoogleStrategy({
-            clientID: googleConfig.googleKey,
-            clientSecret: googleConfig.googleSecret,
-            callbackURL: "http://localhost:3000/index.html"    
-        }, function (accessToken, refreshToken, profile, done) {
-            googleUser.googleId = profile.id;
-            googleUser.googleProfile = profile;
-            googleUser.googleAccessToken = accessToken;
-        });
-        
+        var googleStrategy = new GoogleStrategy(
+            {
+                clientID: googleConfig.googleKey,
+                clientSecret: googleConfig.googleSecret,
+                callbackURL: 'http://localhost:3000'
+            },
+            function (accessToken, refreshToken, profile, done) {
+                googleUser.googleId = profile.id;
+                googleUser.googleProfile = profile;
+                googleUser.googleAccessToken = accessToken;
+            }
+        );
+
         passport.use(googleStrategy);
 
         //authenticate user through google redirect
-        app.get('/auth/google', passport.authenticate('google', {scope:['https://www.googleapis.com/auth/userinfo.profile']}));
+        app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile'] }));
+    });
+
+    socket.on('create post', function () {
+        console.log('user wants to create post');
     });
 });
 
