d63501647727ad7ba38a8ea91e9d610a3d463475
Jeffrey Lansford <lansfordjr14@gmail.com>
2020-05-02 19:09:43 -0600

    fixed misspell error on require line for mongoose-findorcreate

--- models/user.js
+++ models/user.js
@@ -1,19 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const uniqueValidator = require('mongoose-unique-validator');
-const findOrCreate = require('mogoose-findorcreate')
+const findOrCreate = require('mongoose-findorcreate');
 
 const UserSchema = new Schema({
     username: { type: String },
     googleId: { type: String },
     googleProfile: { type: Object },
-    googleAccessToken: { type: String } 
-
+    googleAccessToken: { type: String }
 });
 
-UserSchema.index({username: 1, googleId: 1}, {unique: true}); //Defines an index (most likely compound) for this schema
+UserSchema.index({ username: 1, googleId: 1 }, { unique: true }); //Defines an index (most likely compound) for this schema
 UserSchema.plugin(uniqueValidator);
 UserSchema.plugin(findOrCreate);
 
-const User = mongoose.model("User", UserSchema);
-module.exports = User; 
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
