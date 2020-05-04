5e93f5aac024066b06b482f18d7e30fa9c87bb81
Jeffrey Lansford <lansfordjr14@gmail.com>
2020-05-04 15:18:29 -0600

    added socket.io implementation for request posts on client side and server sending data to client and display it

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = 3000;
 
-
 /* google OAuth set up */
 const googleConfig = require('./OAuth/config');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 /* mongoose set up */
-mongoose.connect('mongodb+srv://Gerg:getrekt@webusers-5lz1h.mongodb.net/GetRekt?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).catch(error => console.log("Connection Error: " + error));
+mongoose
+    .connect('mongodb+srv://Gerg:getrekt@webusers-5lz1h.mongodb.net/GetRekt?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch((error) => console.log('Connection Error: ' + error));
 mongoose.connection.on('error', function (err) {
     console.error('MongoDB error: ' + err.message);
-    console.error('Make sure a mongoDB server is running and accessible by this application')
-  });
+    console.error('Make sure a mongoDB server is running and accessible by this application');
+});
 var User = require('./models/user');
 
 app.use(express.static('public'));
@@ -25,11 +26,11 @@ const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user._id);
 });
 
-passport.deserializeUser(function(userId, done) {
+passport.deserializeUser(function (userId, done) {
     User.findById(userId, (err, user) => done(err, user));
 });
 
@@ -40,35 +41,37 @@ io.on('connection', function (socket) {
     /* google login */
     socket.on('googleLogin', function () {
         console.log('a user wants to login');
-        var googleStrategy = new GoogleStrategy({
-            clientID: googleConfig.googleKey,
-            clientSecret: googleConfig.googleSecret,
-            /* Switch callbackURL to localhost for direct local testing,
-            *  after testing, switch callbackURL back to gamergetrekt.com then
-            *  push your updated code for Jeff to pull
-            */
-            callbackURL: "http://localhost:3000/auth/google/callback"
-            //callbackURL: "http://gamergetrekt.com:3000" 
-        }, function (accessToken, refreshToken, profile, done) {
-            // find or create user from the database based on googleId and google display name
-            User.findOrCreate({googleId: profile.id}, function(err ,user){
-                console.log("finding user...");
-                if(!err){
-                    //save google profile and access token to the user.js object
-                    user.googleProfile = profile;
-                    user.googleAccessToken = accessToken;
-                    user.save(function(err){
+        var googleStrategy = new GoogleStrategy(
+            {
+                clientID: googleConfig.googleKey,
+                clientSecret: googleConfig.googleSecret,
+                /* Switch callbackURL to localhost for direct local testing,
+                 *  after testing, switch callbackURL back to gamergetrekt.com then
+                 *  push your updated code for Jeff to pull
+                 */
+                callbackURL: 'http://localhost:3000/auth/google/callback'
+                //callbackURL: "http://gamergetrekt.com:3000"
+            },
+            function (accessToken, refreshToken, profile, done) {
+                // find or create user from the database based on googleId and google display name
+                User.findOrCreate({ googleId: profile.id }, function (err, user) {
+                    console.log('finding user...');
+                    if (!err) {
+                        //save google profile and access token to the user.js object
+                        user.googleProfile = profile;
+                        user.googleAccessToken = accessToken;
+                        user.save(function (err) {
+                            done(err, user);
+                            console.log('Saved user profile to database' + user);
+                        });
+                    } else {
                         done(err, user);
-                        console.log("Saved user profile to database" + user);
-                    });
-                } else {
-                   done(err, user); 
-                }
-            });
-        });
-        
-        passport.use(googleStrategy);
+                    }
+                });
+            }
+        );
 
+        passport.use(googleStrategy);
     });
 
     socket.on('create post', function (data) {
@@ -80,8 +83,13 @@ io.on('connection', function (socket) {
         console.log('a user wants to request their profile');
         socket.emit('sending user profile', { data: 'Profile' });
     });
-});
 
+    socket.on('request posts', function () {
+        console.log('a user wants to see posts');
+        //query posts
+        socket.emit('receive posts', { text: 'Example Posts' });
+    });
+});
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + 'public/index.html');
@@ -90,11 +98,9 @@ app.get('/', function (req, res) {
 //authenticate user through google redirect
 app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile'] }));
 
-app.get('/auth/google/callback', 
-    passport.authenticate('google', { failureRedirect: '/auth/google'}),
-    function(req, res){
-        res.redirect('/');
-    });
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/auth/google' }), function (req, res) {
+    res.redirect('/');
+});
 
 http.listen(port, function () {
     console.log(`listening on port ${port}`);
--- public/web_scripts/homePage.js
+++ public/web_scripts/homePage.js
@@ -1,5 +1,9 @@
 const socket = io();
 
+window.onload = function () {
+    socket.emit('request posts');
+};
+
 function is_url(str) {
     regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
     if (regexp.test(str)) {
@@ -10,7 +14,7 @@ function is_url(str) {
 }
 
 function login() {
-    socket.emit('login'); 
+    socket.emit('login');
 }
 
 function googleLogin() {
@@ -37,3 +41,28 @@ function createPost() {
         return false;
     }
 }
+
+function makeHTMLPost(parent, data) {
+    var child = document.createElement('div');
+    var post = document.createElement('p');
+    post.textContent = data.text;
+    child.appendChild(post);
+    parent.appendChild(child);
+}
+
+function getPosts(data) {
+    try {
+        var feed = document.getElementById('feed');
+
+        // create the html object with the post information
+        makeHTMLPost(feed, data);
+    } catch (err) {
+        alert(err);
+        console.log('oof');
+    }
+}
+
+socket.on('receive posts', function (data) {
+    console.log('got posts');
+    getPosts(data);
+});
